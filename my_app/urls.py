bdbae1f7fd5e55a07ed1887e05a7969bf75b9c08
habsfanhak <56047585+habsfanhak@users.noreply.github.com>
2023-06-08 17:56:40 -0400

    added paths for students and student

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path('register/student/', views.registerStudent, name='registerStudent'),
     path('register/course/', views.registerCourse, name='registerCourse'),
-    path('register/grade/', views.registerCourse, name='registerGrade'),
+    path('register/grade/', views.registerGrade, name='registerGrade'),
+    path('students/', views.students, name='students'),
+    path('students/<int:pk>', views.student, name='student')
 ]

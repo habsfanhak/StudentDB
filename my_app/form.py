7d7ad3fcc86aefacb42a87ccd8ae770cb90c1967
habsfanhak <56047585+habsfanhak@users.noreply.github.com>
2023-06-08 17:56:05 -0400

    added extra validation to grade form

--- my_app/form.py
+++ my_app/form.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Student, Course
+from .models import Student, Course, Grade
+from django.core import validators
 
 class StudentForm(forms.Form):
     email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -10,7 +11,13 @@ class CourseForm(forms.Form):
     course_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     course_description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-class GradeForm(forms.ModelForm):
+class GradeForm(forms.Form):
+    def validate_result(value):
+        if not value.isdigit():
+            raise validators.ValidationError('Enter a valid number.')
+        elif int(value) > 100:
+            raise validators.ValidationError('Number must not exceed 100.')
     student_number = forms.ModelChoiceField(queryset=Student.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
     course_id = forms.ModelChoiceField(queryset=Course.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
-    result = forms.NumberInput(attrs={'class': 'form-control'}),
+    result = forms.CharField(validators=[validate_result], widget=forms.TextInput(attrs={'class': 'form-control'}))
+
